106c8f8b6519ca4adc3f471231386ea15f9f95d6
josh warren <81135679+joshwrn@users.noreply.github.com>
2024-07-02 12:23:31 -0700

    wip

--- src/components/Window/Window.tsx
+++ src/components/Window/Window.tsx
@@ -14,6 +14,7 @@ import { WindowType } from '@/state/windows'
 import { match, P } from 'ts-pattern'
 import { joinClasses } from '@/utils/joinClasses'
 import { RotationPoints } from './RotationPoints'
+import { Point2d } from '@/state'
 
 const matchBody = (
   body: string | Iframe,
@@ -48,6 +49,45 @@ const matchBody = (
     .otherwise(() => null)
 }
 
+function rotatePoint(point: Point2d, angle: number): Point2d {
+  const { x, y } = point
+  // Convert the angle from degrees to radians
+  let radians = angle * (Math.PI / 180)
+
+  // Calculate the new coordinates
+  let xNew = x * Math.cos(radians) - y * Math.sin(radians)
+  let yNew = x * Math.sin(radians) + y * Math.cos(radians)
+
+  return { x: xNew, y: yNew }
+}
+
+function rotatePointAroundCenter(
+  point: Point2d,
+  center: Point2d,
+  angle: number,
+): Point2d {
+  const { x, y } = point
+  const { x: cx, y: cy } = center
+  // Translate point to the origin
+  let translatedX = x - cx
+  let translatedY = y - cy
+
+  // Rotate the point
+  let rotatedPoint = rotatePoint(
+    {
+      x: translatedX,
+      y: translatedY,
+    },
+    angle,
+  )
+
+  // Translate point back
+  let xNew = rotatedPoint.x + cx
+  let yNew = rotatedPoint.y + cy
+
+  return { x: xNew, y: yNew }
+}
+
 export const WindowInternal: FC<{
   item: Item
   window: WindowType
@@ -64,6 +104,8 @@ export const WindowInternal: FC<{
       setHoveredWindow: state.setHoveredWindow,
       snapToWindows: state.snapToWindows,
       setSnappingToPositions: state.setSnapLines,
+      spaceMousePosition: state.spaceMousePosition,
+      zoom: state.zoom,
     })),
   )
 
@@ -72,6 +114,10 @@ export const WindowInternal: FC<{
   const { width, height } = window
 
   const nodeRef = React.useRef<HTMLDivElement>(null)
+  const mainRef = React.useRef<HTMLDivElement>(null)
+  const counterRef = React.useRef<HTMLDivElement>(null)
+  const canvasRef = React.useRef<HTMLCanvasElement>(null)
+  const lastPosition = React.useRef({ x: 0, y: 0 })
 
   const onDrag = (e: DraggableEvent, data: DraggableData) => {
     if (!(e instanceof MouseEvent)) return
@@ -105,6 +151,11 @@ export const WindowInternal: FC<{
     () => state.connections.filter((c) => c.from === item.id),
     [state.connections, item.id],
   )
+
+  const [centerPoint, setCenterPoint] = React.useState<Point2d>({
+    x: 0,
+    y: 0,
+  })
 
   return (
     <DraggableCore
@@ -163,8 +214,75 @@ export const WindowInternal: FC<{
           </section>
         </header>
 
-        <main className={styles.content}>
-          {item.body.map((body, i) => matchBody(body, i))}
+        <main className={styles.content} ref={mainRef}>
+          {/* {item.body.map((body, i) => matchBody(body, i))} */}
+          <div
+            style={{
+              width: '20px',
+              height: '20px',
+              backgroundColor: 'blue',
+              position: 'absolute',
+              transform: `translate(${centerPoint.x}px, ${centerPoint.y}px)`,
+            }}
+          />
+          <div
+            data-role="counter-rect"
+            style={{
+              width: `${width}px`,
+              height: `${height - 110}px`,
+              position: 'absolute',
+              transform: `rotate(${-window.rotation}deg)`,
+              pointerEvents: 'none',
+              border: '1px solid red',
+            }}
+            ref={counterRef}
+          />
+          <canvas
+            width={width}
+            height={height - 110}
+            style={{
+              position: 'relative',
+              top: 0,
+              left: 0,
+              border: '1px solid blue',
+            }}
+            ref={canvasRef}
+            onMouseMove={(e) => {
+              const ctx = canvasRef.current?.getContext('2d')
+              const counterBox = counterRef.current?.getBoundingClientRect()
+              if (!ctx) return
+              if (!counterBox) return
+              if (!mainRef.current) return
+              const center = {
+                x: counterBox.width / 2 / state.zoom,
+                y: counterBox.height / 2 / state.zoom,
+              }
+              setCenterPoint(center)
+              const mousePositionPure = {
+                x: (e.clientX - counterBox.left) / state.zoom,
+                y:
+                  (e.clientY - counterBox.top) / state.zoom +
+                  mainRef.current.scrollTop,
+              }
+              const rotatedMousePosition = rotatePointAroundCenter(
+                mousePositionPure,
+                center,
+                -window.rotation,
+              )
+              console.log(rotatedMousePosition, mousePositionPure, center)
+
+              const from = lastPosition.current
+              ctx.beginPath()
+              ctx.lineWidth = 3
+              ctx.lineCap = `round`
+              ctx.strokeStyle = 'red'
+              ctx.moveTo(from.x, from.y)
+              ctx.lineTo(rotatedMousePosition.x, rotatedMousePosition.y)
+              ctx.stroke()
+
+              lastPosition.current = rotatedMousePosition
+            }}
+          />
         </main>
         <ConnectorOverlay id={item.id} />
 
