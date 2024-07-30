5b7195970c58c103a24bad860c7e0113ec05566c
josh warren <81135679+joshwrn@users.noreply.github.com>
2024-07-29 22:35:17 -0700

    remove openai endpoint

--- src/server/mockedEndpoints.ts
+++ src/server/mockedEndpoints.ts
@@ -1,9 +1,6 @@
 import { ApiRouteUrl } from './routes'
 
 export const mockedEndpoints: Record<ApiRouteUrl, { [key: string]: boolean }> = {
-  generateImage: {
-    post: false,
-  },
   imageUrlToBase64: {
     get: false,
   },
--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,6 +1,5 @@
 export const API_ROUTES = [
   'fal/proxy',
-  'generateImage',
   'imageUrlToBase64',
 ] as const
 export type ApiRouteUrl = (typeof API_ROUTES)[number]
