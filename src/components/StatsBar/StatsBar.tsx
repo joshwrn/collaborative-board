c92df3a284198d2bc1de8154ce75b5c46c52243b
josh warren <81135679+joshwrn@users.noreply.github.com>
2024-08-17 11:27:22 -0700

    fix initial animation statbar

--- src/components/StatsBar/StatsBar.tsx
+++ src/components/StatsBar/StatsBar.tsx
@@ -30,19 +30,21 @@ const StatsBar_Internal: React.FC = () => {
       </motion.div>
       <motion.div className={style.stat}>
         <RiRefreshLine size={17} stroke={`var(--white-65)`} />
-        <motion.p
-          layout
-          key={`${state.generatingCanvas.length}`}
-          initial={{ opacity: 0, y: 10 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -10, transition: { duration: 0.2 } }}
-          transition={{
-            type: `spring`,
-            duration: 0.5,
-          }}
-        >
-          {state.generatingCanvas.length}
-        </motion.p>
+        <AnimatePresence mode="wait" initial={false}>
+          <motion.p
+            layout
+            key={`${state.generatingCanvas.length}`}
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10, transition: { duration: 0.2 } }}
+            transition={{
+              type: `spring`,
+              duration: 0.5,
+            }}
+          >
+            {state.generatingCanvas.length ?? 0}
+          </motion.p>
+        </AnimatePresence>
       </motion.div>
     </div>
   )
